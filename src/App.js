0535764735332fb5b8c8dc48317cb7abd8f8d8d1
Kartik Malik <kartikforhach@gmail.com>
2020-08-07 02:08:46 +0530

    Post task changes

--- src/App.js
+++ src/App.js
@@ -17,28 +17,50 @@ const App = () => {
   const [r3,setR3] = useState(false)
   const [r4,setR4] = useState(false)
   const [r5,setR5] = useState(false)
+  const [travled1,setTravled1] = useState(false)
+  const [travled2,setTravled2] = useState(false)
+  const [travled3,setTravled3] = useState(false)
+  const [travled4,setTravled4] = useState(false)
+  const [travled5,setTravled5] = useState(false)
+  const [flagged,setFlagged] = useState(false)
   const [pathColor,setPathColor] = useState("#000")
   let num = 0;
   let path = new Array(76).fill(0);
   const launchR1 = () =>{
+    if(travled1) return;
     setR1(true)
+    setTravled1(true)
     setPathColor("#0d0")
   }
   const launchR2 = () =>{
+    if(travled2) return;
     setR2(true)
+    setTravled2(true)
     setPathColor("#01e")
   }
   const launchR3 = () =>{
+    if(travled3) return;
     setR3(true)
+    setTravled3(true)
     setPathColor("#00e")
   }
   const launchR4 = () =>{
+    if(travled4) return;
     setR4(true)
+    setTravled4(true)
     setPathColor("#000")
   }
   const launchR5 = () =>{
+    if(travled5) return;
     setR5(true)
+    setTravled5(true)
     setPathColor("#a00")
+  }
+  if(travled1 && travled2 && travled3 && travled4 && travled5 && !flagged){
+    setFlagged(true)
+    setTimeout(() => {
+      alert("Congratulations! all Rafals have arrived.")
+    }, 3500);
   }
   return (
     <>
@@ -57,7 +79,7 @@ const App = () => {
           <Pin size={20} />
         </Marker>
         {path.map(()=>{
-          {num = num+1}
+          num = num+1
           return(
             <Marker
               longitude={2.21+num*1}
@@ -67,11 +89,11 @@ const App = () => {
             </Marker>
           )
         })}
-        {r1?<PlaneTravel color="#0d0"></PlaneTravel>:null}
-        {r2?<PlaneTravel color="#01e"></PlaneTravel>:null}
-        {r3?<PlaneTravel color="#00e"></PlaneTravel>:null}
-        {r4?<PlaneTravel color="#000"></PlaneTravel>:null}
-        {r5?<PlaneTravel color="#a00"></PlaneTravel>:null}
+        {r1?<PlaneTravel color="#0d0" setOnRoute={setR1}></PlaneTravel>:null}
+        {r2?<PlaneTravel color="#01e" setOnRoute={setR2}></PlaneTravel>:null}
+        {r3?<PlaneTravel color="#00e" setOnRoute={setR3}></PlaneTravel>:null}
+        {r4?<PlaneTravel color="#000" setOnRoute={setR4}></PlaneTravel>:null}
+        {r5?<PlaneTravel color="#a00" setOnRoute={setR5}></PlaneTravel>:null}
         <Marker
           longitude={76.7821}
           latitude={30.3752}
@@ -79,11 +101,11 @@ const App = () => {
           <Pin size={20} />
         </Marker>
     </MapGL>
-    <button onClick={launchR1}>Lanch Rafale 1</button>
-    <button onClick={launchR2}>Lanch Rafale 2</button>
-    <button onClick={launchR3}>Lanch Rafale 3</button>
-    <button onClick={launchR4}>Lanch Rafale 4</button>
-    <button onClick={launchR5}>Lanch Rafale 5</button>
+    <button onClick={launchR1} disabled={r1||r2||r3||r4||r5}>Lanch Rafale 1</button>
+    <button onClick={launchR2} disabled={r1||r2||r3||r4||r5}>Lanch Rafale 2</button>
+    <button onClick={launchR3} disabled={r1||r2||r3||r4||r5}>Lanch Rafale 3</button>
+    <button onClick={launchR4} disabled={r1||r2||r3||r4||r5}>Lanch Rafale 4</button>
+    <button onClick={launchR5} disabled={r1||r2||r3||r4||r5}>Lanch Rafale 5</button>
     </>
   );
 }
--- src/planeTravel.js
+++ src/planeTravel.js
@@ -2,25 +2,36 @@ import React from 'react'
 import {Marker} from 'react-map-gl';
 import Plane from './plane'
 const PlaneTravel = (props) =>{
-    let path = new Array(76).fill(0);
     const [pos,setPos] = React.useState({
         lon: 2.21,
         lat: 46.22,
         index: 0,
-        size: -25
+        size: -25,
+        rotation: -60
     })
     const mov = () =>{
         const num = pos.index
         if(num<150){
-            setPos({lon: pos.lon+1/2,lat:pos.lat-0.21/2,index:num+1,size:pos.size+0.33})
+            setPos({...pos,lon: pos.lon+1/2,lat:pos.lat-0.21/2,index:num+1,size:pos.size+0.33})
+        }else{
+            props.setOnRoute(false)
         }
       }
+    const rot = () =>{
+        if(pos.rotation<60){
+            setPos({...pos,rotation:pos.rotation+2})
+        }else{
+            setPos({...pos,index:pos.index+1})
+        }
+    }
     React.useEffect(() => {
     const timer = setTimeout(() => {
-        if(pos.index<=150){
+        if(pos.index === 0){
+            rot()
+        }else if(pos.index<=150){
             mov()
         }
-    }, 30);
+    }, 10);
     return () => clearTimeout(timer);
     }, [pos]);
     return(
@@ -28,7 +39,7 @@ const PlaneTravel = (props) =>{
           longitude={pos.lon}
           latitude={pos.lat}
         >
-            <Plane color={props.color} size={50-Math.abs(pos.size)}></Plane>
+            <Plane color={props.color} size={50-Math.abs(pos.size)} angle={pos.rotation}></Plane>
         </Marker>
     )
 }
